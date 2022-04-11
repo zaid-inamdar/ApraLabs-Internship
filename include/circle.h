2cc65897954b63a2bc9a729f76c41ae4881442cb
Zaid Inamdar <76680580+zaid-inamdar@users.noreply.github.com>
2022-04-11 15:47:32 +0530

    Add files via upload

--- include/circle.h
+++ include/circle.h
@@ -1,1 +1,26 @@
+#pragma once
 
+
+#include<iostream>
+#include<string.h>
+#include"Shape.h"
+
+using namespace std;
+
+class Circle : Shape {
+public:
+	double radius;
+	string color;
+public:
+	string getType();
+
+	//getRadius();
+
+	double getRadius();
+
+	double getPerimeter();
+
+	double getArea();
+
+	string getColor();
+};
